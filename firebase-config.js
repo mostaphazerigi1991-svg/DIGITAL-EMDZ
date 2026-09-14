eeeff470d7125ef61f704e0a382ac5eef11cb07d
mostaphazerigi1991-svg <mostaphazerigi1991@gmail.com>
2026-09-14 21:02:15 +0100

    Connect DIGITAL EMDZ to Firebase

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration for DIGITAL EMDZ Store.
+// This web config is intended for browser use. Never put service-account credentials here.
+export const firebaseConfig = {
+  apiKey: "AIzaSyC1kMVqsq-5PYwS5rylWq_2SHnmznWPr1s",
+  authDomain: "digital-emdz.firebaseapp.com",
+  projectId: "digital-emdz",
+  storageBucket: "digital-emdz.firebasestorage.app",
+  messagingSenderId: "441778566344",
+  appId: "1:441778566344:web:49566d9ab7e155e1aaa1fd"
+};
