c987720ad1ae4c18067623843bc6dc87d09762d6
mostaphazerigi1991-svg <mostaphazerigi1991@gmail.com>
2026-09-14 20:13:31 +0100

    Add Firebase configuration template

---
+++ firebase-config.example.js
@@ -0,0 +1,3 @@
+// انسخ هذا الملف إلى firebase-config.js بعد إنشاء مشروع Firebase.
+// لا تضع كلمات المرور أو Service Account keys في GitHub.
+export const firebaseConfig={apiKey:'YOUR_API_KEY',authDomain:'YOUR_PROJECT.firebaseapp.com',projectId:'YOUR_PROJECT_ID',storageBucket:'YOUR_PROJECT.appspot.com',messagingSenderId:'YOUR_SENDER_ID',appId:'YOUR_APP_ID'};
